29888ecdd4afa47a371ce58cf543efa6ab479c9d
Dipesh <dipeshpansuriya@ymail.com>
2021-07-11 23:01:28 +0530

    API Change

--- Application_API/Controllers/UserConfig/RoleController.cs
+++ Application_API/Controllers/UserConfig/RoleController.cs
@@ -5,25 +5,25 @@ using System.Threading.Tasks;
 
 namespace Application_API.Controllers.UserConfig
 {
-    public class UserController : BaseController<UserController>
+    public class RoleController : BaseController<RoleController>
     {
         [HttpGet]
         public async Task<ActionResult> GetAll()
         {
-            Response res = await this.Mediator.Send(new User_Inst_cmd());
+            Response res = await this.Mediator.Send(new Role_Inst_cmd());
 
             return this.Ok(res);
         }
 
         [HttpPost]
-        public async Task<ActionResult<Response>> Create([FromBody] User_Inst_cmd inst_Cmd)
+        public async Task<ActionResult<Response>> Create([FromBody] Role_Inst_cmd inst_Cmd)
         {
             Response res = await this.Mediator.Send(inst_Cmd);
             return this.Ok(res);
         }
 
         [HttpPut]
-        public async Task<ActionResult<Response>> Update([FromBody] User_Upd_cmd upd_Cmd)
+        public async Task<ActionResult<Response>> Update([FromBody] Role_Upd_cmd upd_Cmd)
         {
             Response res = await this.Mediator.Send(upd_Cmd);
             return this.Ok(res);
@@ -32,7 +32,7 @@ namespace Application_API.Controllers.UserConfig
         [HttpPut]
         public async Task<ActionResult<Response>> Delete(int Id)
         {
-            Response res = await this.Mediator.Send(new User_Del_cmd { Id = Id });
+            Response res = await this.Mediator.Send(new Role_Del_cmd { Id = Id });
             return this.Ok(res);
         }
 
