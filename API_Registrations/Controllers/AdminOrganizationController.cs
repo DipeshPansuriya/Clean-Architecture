5367a0a0dd53d0a8eaece38dd92cf7f4db3da132
Dipesh Pansuriya <dipeshpansuriya@ymail.com>
2022-02-28 21:55:30 +0530

    Insert AdminOrganization

---
+++ API_Registrations/Controllers/AdminOrganizationController.cs
@@ -0,0 +1,24 @@
+﻿using Application_Services;
+using Microsoft.AspNetCore.Mvc;
+using Registrations_Command.AdminOrganization.Insert;
+
+namespace API_Registrations.Controllers
+{
+    public class AdminOrganizationController : BaseController<AdminOrganizationController>
+    {
+        [HttpPost]
+        public async Task<ActionResult<Response>> Create([FromBody] Adm_Org_Inst inst_Cmd)
+        {
+            Response res = await Mediator.Send(inst_Cmd);
+
+            if (!res.ResponseStatus)
+            {
+                return BadRequest(res);
+            }
+            else
+            {
+                return Ok(res);
+            }
+        }
+    }
+}
